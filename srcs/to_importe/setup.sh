63eecfcf33cc559337c5566f4173cfa9b021da12
maxime-42 <mkayumba@student.42.fr>
2021-12-27 19:02:00 +0100

    add dockerfile nginx

---
+++ srcs/to_importe/setup.sh
@@ -0,0 +1,11 @@
+########### wordpress ##############
+echo "########### wordpresss config ###########\n"
+# mkdir /var/www/wordpress
+# cp index.html /var/www/wordpress
+########### config nginx ###########
+echo "########### config nginx ###########\n"
+# chown -R www-data:www-data /var/www/*
+# chmod -R 755 /var/www/*
+# cat /srcs/nginx.conf > /etc/nginx/sites-available/default
+# service nginx start
+# nginx -g daemon off
