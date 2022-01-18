fadc75b450fbe39179914d49e6e4eeaf18008e1c
maxime-42 <mkayumba@student.42.fr>
2022-01-18 10:56:43 +0100

    backup mysql is ok

--- srcs/mariadb/start.sh
+++ srcs/mariadb/start.sh
@@ -2,30 +2,35 @@
 
 
 # mysqld --skip-networking &
-echo "\n[i]Initialization of database\n"
-mysql_install_db --datadir=$DATADIR    > /dev/null
+if [ ! -d $DATADIR/mysql ]; then #check if database is not already created
+	echo "\n[i]Initialization of database\n"
+	mysql_install_db --datadir=$DATADIR    > /dev/null
 
-echo "\n[i] start === mysqld_safe === is startup script is in MariaDB distributions on Linux and Unix. 
-It is a wrapper that starts mysqld with some extra safety features. 
-For example, if mysqld_safe notices that mysqld has crashed, then mysqld_safe will automatically restart mysqld.\n"
-mysqld_safe &
+	echo "\n[i] start === mysqld_safe === is startup script is in MariaDB distributions on Linux and Unix. 
+	It is a wrapper that starts mysqld with some extra safety features. 
+	For example, if mysqld_safe notices that mysqld has crashed, then mysqld_safe will automatically restart mysqld.\n"
+	mysqld_safe &
 
-echo "\n[i] Create user\n"
-sleep 1
-mysql -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};"
-mysql -e "CREATE USER IF NOT EXISTS ${WP_DB_USER}@'%' IDENTIFIED BY '${WP_DB_USER}';"
-mysql -e "CREATE USER IF NOT EXISTS ${WP_ADMIN_USER}@'%' IDENTIFIED BY '${WP_ADMIN_USER}';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO ${WP_ADMIN_USER}@'%' IDENTIFIED BY '${WP_ADMIN_USER}';"
-mysql -e "GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO ${WP_DB_USER}@'%';"
-mysql -e "FLUSH PRIVILEGES;"
+	echo "[i] Create user\n"
+	sleep 1
+	mysql -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};"
+	mysql -e "CREATE USER IF NOT EXISTS ${WP_DB_USER}@'%' IDENTIFIED BY '${WP_DB_USER}';"
+	mysql -e "CREATE USER IF NOT EXISTS ${WP_ADMIN_USER}@'%' IDENTIFIED BY '${WP_ADMIN_USER}';"
+	mysql -e "GRANT ALL PRIVILEGES ON *.* TO ${WP_ADMIN_USER}@'%' IDENTIFIED BY '${WP_ADMIN_USER}';"
+	mysql -e "GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO ${WP_DB_USER}@'%';"
+	mysql -e "FLUSH PRIVILEGES;"
 
-#GRANT ALL ON wordpress.* TO 'admin'@'localhost' IDENTIFIED BY 'admin' WITH GRANT OPTION;
+	echo "\n[i] Insert Wordpress database backup\n"
+	mysql -u root wordpress < ./wp_backup_db.sql
+	echo "[i] database shutdown\n"
+	sleep 2
+	mysqladmin shutdown
 
-echo "[i] database shutdown\n"
-sleep 2
-mysqladmin shutdown
-
-echo "[i] database restarting\n"
-sleep 2
-
+	echo "[i] database restarting\n"
+	sleep 2
+else
+	echo "[i] Skipping initialization. Mysql database already created\n"
+fi
+echo "[i] Starting mariadb server"
 exec mysqld -u root
+
